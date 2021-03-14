c9bd02a32413f39ae16c147930e892e342e328f3
vats1812 <68377043+vats1812@users.noreply.github.com>
2021-03-14 13:05:42 +0530

    Create index.js

---
+++ smiley/index.js
@@ -0,0 +1,9 @@
+function good() {
+    document.getElementById("good").style.display = "flex";
+    document.getElementById("devil").style.display = "none";
+}
+
+function devil() {
+    document.getElementById("good").style.display = "none";
+    document.getElementById("devil").style.display = "flex";
+}
